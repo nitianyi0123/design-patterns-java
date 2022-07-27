b6b965d1283faceee3c418feae41757d444fb221
nitianyi <nitianyi@shouqianba.com>
2022-07-27 15:13:15 +0800

    rename

--- src/main/java/com/tj/designpatterns/DesignPatternsApplication.java
+++ src/main/java/com/tj/designpatterns/DesignPatternsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesignPatternsDemoApplication {
+public class DesignPatternsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesignPatternsDemoApplication.class, args);
+		SpringApplication.run(DesignPatternsApplication.class, args);
 	}
 
 }
